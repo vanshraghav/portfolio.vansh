1a159a340875987d5881de51b61063dbb0a47b14
Vansh Raghav <114331443+vanshraghav@users.noreply.github.com>
2023-09-25 00:47:59 +0530

    Update mycard.js

--- src/components/portfolio/mycard.js
+++ src/components/portfolio/mycard.js
@@ -5,11 +5,13 @@ const Mycard = (props) => {
   const { imgSrc, heading, link, desc } = props;
 
   return (
-    <div className="mycard">
-      <img src={imgSrc} alt="Card" className="card-image" />
-      <h2 className="card-heading">{heading}</h2>
-      <p className="card-para">{desc}</p>
-    </div>
+    <a href={link} target="_blank" rel="noopener noreferrer" className="mycard">
+      <div className="mycard">
+        <img src={imgSrc} alt="Card" className="card-image" />
+        <h2 className="card-heading">{heading}</h2>
+        <p className="card-para">{desc}</p>
+      </div>
+    </a>
   );
 };
 
